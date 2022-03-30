c2862049d4a9a522dcad70032d241003e81b576d
Knuutti <96994953+Knuutti@users.noreply.github.com>
2022-03-30 13:06:19 +0300

    Viikko 10
    Initial commit

--- app/src/main/java/com/example/webselain/MainActivity.java
+++ app/src/main/java/com/example/webselain/MainActivity.java
@@ -8,10 +8,15 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    WebView wvWeb;
-    EditText etAddress;
+    private WebView wvWeb;
+    private EditText etAddress;
+    private ArrayList<Page> pageHistory = new ArrayList<>();
+    private String url;
+    private int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +24,84 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         wvWeb = findViewById(R.id.wvWeb);
+
         etAddress = findViewById(R.id.etAddress);
 
         wvWeb.getSettings().setJavaScriptEnabled(true);
         wvWeb.setWebViewClient(new WebViewClient());
-        wvWeb.loadUrl("http://www.lut.fi");
 
+        // Default page
+        url = "http://www.lut.fi";
+        wvWeb.loadUrl(url);
+        pageHistory.add(new Page(url));
+        position = 0;
 
     }
 
+    // Search
     public void loadAddress(View v) {
-        String url = etAddress.getText().toString();
-        System.out.println(url);
+
+        // Gets the address from search bar
+        url = etAddress.getText().toString();
+
+        // Loading the page
+        String[] urlArray = url.split("www", 2);
+        if (urlArray[0].length() == 0) {
+            url = "http://www" + url;
+        }
+        else if (urlArray[0].compareTo("index.html") == 0) {
+            url = "file:///android_asset/" + url;
+        }
         wvWeb.loadUrl(url);
 
+        // Managing page history
+        addPageToHistory(new Page(url));
+    }
+
+    private void addPageToHistory(Page page) {
+
+        // If page history is full, remove the oldest page
+        if (position > 9) {
+            pageHistory.remove(0);
+        }
+        // Otherwise move the selected position
+        else {
+            position++;
+        }
+
+        // If user searches for new page after going back to previous page,
+        // remove the pages after that page
+        if (position < pageHistory.size() - 1) {
+            for (int i = position; i < pageHistory.size(); i++) {
+                pageHistory.remove(position);
+            }
+        }
+
+        // Adds the page to the list
+        pageHistory.add(page);
+    }
+
+    public void nextPage(View v) {
+        if (position < pageHistory.size() - 1) {
+            position++;
+            wvWeb.loadUrl(pageHistory.get(position).getUrl());
+        }
+    }
+
+    public void previousPage(View v) {
+        if (position > 0) {
+            position--;
+            wvWeb.loadUrl(pageHistory.get(position).getUrl());
+        }
+    }
+
+
+
+    public void refreshPage(View v) {
+        wvWeb.loadUrl(wvWeb.getUrl());
+    }
+
+    public void executeJavaScript(View v) {
+        wvWeb.evaluateJavascript("javascript:shoutOut()", null);
     }
 }
---
+++ app/src/main/java/com/example/webselain/Page.java
@@ -0,0 +1,17 @@
+package com.example.webselain;
+
+public class Page {
+    private String url;
+
+    public Page(String url){
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
